cde6673c9856f6e1fdabb01a09ccfb29fd82fc6c
ixilai <118871138+ixilai@users.noreply.github.com>
2025-12-05 17:17:22 +0800

    Update loss_vif.py

--- utils22/loss_vif.py
+++ utils22/loss_vif.py
@@ -121,6 +121,5 @@ class fusion_loss_vif(nn.Module):
         loss_l1 = 10 * self.L_Inten(image_A, image_B, image_fused)
         loss_gradient = 10 * self.L_Grad(image_A, image_B, image_fused)
         loss_SSIM = 10 * (1 - self.L_SSIM(image_A, image_B, image_fused))
-        loss_color = 10 * self.loss_func_color(image_RGB, image_fused_RGB)
-        fusion_loss = loss_l1 + loss_gradient + loss_SSIM + loss_color
+        fusion_loss = loss_l1 + loss_gradient + loss_SSIM
         return fusion_loss
